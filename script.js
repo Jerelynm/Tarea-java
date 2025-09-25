7be525fe1c479b18a67e4932ddabbb605f355c58
Jerelynm <jmarinm4@miumg.edu.gt>
2025-09-24 19:15:24 -0600

    Se crea el index.html

---
+++ script.js
@@ -0,0 +1,14 @@
+window.onload = function() {
+alert ( "Estas ingresando a la aplicación"); 
+}
+function otraFuncion(){
+console.log("Otra función ejecutada");
+}
+
+function mostrarVariable(){
+var miVariable = "Hola, soy una variable local";
+var miVariableString = "Hola, soy una variable de tipo string";
+var variableArray = [1, 2, 3, 4, 5];
+console.log(miVariable);
+console.log(miVariableString);
+}
